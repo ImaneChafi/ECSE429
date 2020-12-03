8e1141f91179312cdd992d478563fe3859465793
mahroo12 <mahroo.rahman@gmail.com>
2020-12-02 22:14:44 -0500

    added categories logarithmically

--- src/performance_test_categories.java
+++ src/performance_test_categories.java
@@ -35,9 +35,9 @@ public class performance_test_categories {
 		long T2 = (T2_end - start_t2);
 
 		//Printing the statements to have a record of the performance time
-		System.out.print("\nT1 for test_categiries_post: " + T1);
+		//System.out.print("\nT1 for test_categiries_post: " + T1);//
 
-		System.out.print("\nT2 for test_categories_post: " + T2);
+		System.out.print("\nT2 for test_categories_post: " + T2);//
 		System.out.print("\n");
 	}
 
@@ -59,9 +59,9 @@ public class performance_test_categories {
 		long T1_end = System.nanoTime();
 		long T1 = (T1_end - start_t1);
 		long T2 = (T2_end - start_t2);
-				
+
 		//Printing the statements to have a record of the performance time
-		System.out.print("\nT1 for test_categories_delete: " + T1);
+		//System.out.print("\nT1 for test_categories_delete: " + T1);
 		System.out.print("\nT2 for test_categories_delete: " + T2);
 		System.out.print("\n");
 	}
@@ -88,13 +88,13 @@ public class performance_test_categories {
 		HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
 		//        System.out.println(response.body());
 		long T2_end = System.nanoTime();
-		
+
 		long T1_end = System.nanoTime();
 		long T1 = (T1_end - start_t1);
 		long T2 = (T2_end - start_t2);
-				
+
 		//Printing the statements to have a record of the performance time
-		System.out.print("\nT1 for test_categories_put_works: " + T1);
+		//System.out.print("\nT1 for test_categories_put_works: " + T1);
 		System.out.print("\nT2 for test_categories_put_works: " + T2);
 		System.out.print("\n");
 	}
@@ -106,22 +106,62 @@ public class performance_test_categories {
 	 */
 	public static void main(String[] args) throws Exception {
 
-		int num = 50;       // number of instances to generate, you can change the number to test the performance
+		// testing with 10 values
 
+		int num = 10;
 
-
-
+		System.out.println("Creating " + num + " categories");
 		// create categories using random generator
 		for (int i = 0; i <= num; i++){
 			String title = RandomStringUtils.randomAlphabetic(5);
 			String description = RandomStringUtils.randomAlphabetic(20);
 			create_categories(title, description);
 		}
+		// testing with 100 values
+		int num1 = 100;
+		System.out.print("\n");
+		System.out.println("Creating " + num1 + " categories");
+		// create categories using random generator
+		for (int i = 0; i <= num1; i++){
+			String title = RandomStringUtils.randomAlphabetic(5);
+			String description = RandomStringUtils.randomAlphabetic(20);
+			create_categories(title, description);
+		}
+		// testing with 1000 values
+		int num2 = 1000;
+		System.out.print("\n");
+		System.out.println("Creating " + num2 + " categories");
+		// create categories using random generator
+		for (int i = 0; i <= num2; i++){
+			String title = RandomStringUtils.randomAlphabetic(5);
+			String description = RandomStringUtils.randomAlphabetic(20);
+			create_categories(title, description);
+		}
+		// testing with 10000 values
+		int num3 = 10000;
+		System.out.print("\n");
+		System.out.println("Creating " + num3 + " categories");
+		// create categories using random generator
+		for (int i = 0; i <= num3; i++){
+			String title = RandomStringUtils.randomAlphabetic(5);
+			String description = RandomStringUtils.randomAlphabetic(20);
+			create_categories(title, description);
+		}
 
+		// testing with 100000 values
+		int num4 = 100000;
+		System.out.print("\n");
+		System.out.println("Creating " + num4 + " categories");
+		// create categories using random generator
+		for (int i = 0; i <= num4; i++){
+			String title = RandomStringUtils.randomAlphabetic(5);
+			String description = RandomStringUtils.randomAlphabetic(20);
+			create_categories(title, description);
+		}
 		// couple of instances created with known input values to test the functionality of the functions
 
 		create_categories("love", "lyx");           // id = 3
-		create_categories("queen", "x queen lyx");  // id = 4
+		//create_categories("queen", "x queen lyx");  // id = 4
 
 
 		delete_categories(3);
